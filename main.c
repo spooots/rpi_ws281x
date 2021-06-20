09a9ecdf541adf467f3e0cb2e814e430b294afe5
Andreas Schedel <andreas.schedel@gmail.com>
2021-06-20 23:13:39 +0100

    finalize blctrl adaptions - still missing: error handling

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * newtest.c
+ * main.c
  *
  * Copyright (c) 2014 Jeremy Garff <jer @ jers.net>
  *
@@ -59,9 +59,8 @@ static char VERSION[] = "XX.YY.ZZ";
 #define TARGET_FREQ             WS2811_TARGET_FREQ
 #define GPIO_PIN                18
 #define DMA                     10
-//#define STRIP_TYPE            WS2811_STRIP_RGB		// WS2812/SK6812RGB integrated chip+leds
-#define STRIP_TYPE              WS2811_STRIP_GBR		// WS2812/SK6812RGB integrated chip+leds
-//#define STRIP_TYPE            SK6812_STRIP_RGBW		// SK6812RGBW (NOT SK6812RGB)
+
+#define STRIP_TYPE              WS2811_STRIP_GBR		// WS2812/SK6812RGB integrated chip+leds    // rpi: 0x00BBGGRR
 
 #define WIDTH                   8
 #define HEIGHT                  4
@@ -73,9 +72,7 @@ int led_count = LED_COUNT;
 
 int clear_on_exit = 0;
 
-uint8_t Red = 0;
-uint8_t Green = 0;
-uint8_t Blue = 0;
+int enableTest = 0;
 
 ws2811_t ledstring =
 {
@@ -105,25 +102,26 @@ ws2811_t ledstring =
 #define INDEX_GREEN 	8
 #define INDEX_BLUE 	   16
 
+uint8_t Red = 0;
+uint8_t Green = 0;
+uint8_t Blue = 0;
+
 ws2811_led_t *matrix;
 
-
-void matrix_clear(void)
+ws2811_led_t getColorValue(uint8_t red, uint8_t green, uint8_t blue)
 {
-    int x, y;
-
-    for (y = 0; y < (height ); y++)
-    {
-        for (x = 0; x < width; x++)
-        {
-            matrix[y * width + x] = 0;
-        }
-    }
+	return (red << INDEX_RED) | (green << INDEX_GREEN) | (blue << INDEX_BLUE);
 }
+
+int setLEDValue(ws2811_led_t);
 
 static void ctrl_c_handler(int signum)
 {
 	(void)(signum);
+	setLEDValue(getColorValue(0,0,0));
+	ws2811_render(&ledstring);
+	ws2811_fini(&ledstring);
+	exit(-1);
 }
 
 static void setup_handlers(void)
@@ -135,6 +133,22 @@ static void setup_handlers(void)
 
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
+}
+
+void printUsage(char* app)
+{
+	fprintf(stderr, "%s version %s\n", app, VERSION);
+	fprintf(stderr, "Usage: %s \n"
+		"-h (--help)    - this information\n"
+		"-r (--red)   	- set red value [0-255]\n"
+		"-g (--green)   - set green value [0-255]\n"
+		"-b (--blue)  	- set blue value [0-255]\n"
+		"-o (--off)     - switch off display\n"
+		"-c (--clear)   - clear on exit\n"
+		"-d (--dma)     - select DMA channel\n"
+		"-x (--width)   - set width\n"
+		"-y (--height)  - set height\n"
+		, app);
 }
 
 
@@ -154,13 +168,14 @@ void parseargs(int argc, char **argv, ws2811_t *ws2811)
 		{"dma", required_argument, 0, 'd'},
 		{"width", required_argument, 0, 'x'},
 		{"height", required_argument, 0, 'y'},
+		{"test", no_argument, 0, 't'},
 		{0, 0, 0, 0}
 	};
 
 	while (1)
 	{
 		index = 0;
-		c = getopt_long(argc, argv, "r:g:b:ohcd:x:y:", longopts, &index);
+		c = getopt_long(argc, argv, "tr:g:b:ohcd:x:y:", longopts, &index);
 
 		if (c == -1)
 			break;
@@ -168,19 +183,12 @@ void parseargs(int argc, char **argv, ws2811_t *ws2811)
 		switch (c)
 		{
 		case 'h':
-			fprintf(stderr, "%s version %s\n", argv[0], VERSION);
-			fprintf(stderr, "Usage: %s \n"
-				"-h (--help)    - this information\n"
-				"-r (--red)   	- set red value [0-255]\n"
-				"-g (--green)   - set green value [0-255]\n"
-				"-b (--blue)  	- set blue value [0-255]\n"
-				"-o (--off)     - switch off display\n"
-				"-c (--clear)   - clear on exit\n"
-				"-d (--dma)     - select DMA channel\n"
-				"-x (--width)   - set width\n"
-				"-y (--height)  - set height\n"
-				, argv[0]);
+			printUsage(argv[0]);
 			exit(-1);
+
+		case 't':
+			enableTest = 1;
+			break;
 
 		case 'r': {
 			if (optarg) {
@@ -262,85 +270,64 @@ void parseargs(int argc, char **argv, ws2811_t *ws2811)
 			exit(0);
 
 		default:
+			printUsage(argv[0]);
 			exit(-1);
 		}
 	}
+}
+
+int setNextLED(ws2811_led_t value)
+{
+	ledstring.channel[0].leds[0] = value;
+}
+
+int setLEDValue(ws2811_led_t value)
+{
+	for (int i = 0; i < (width * height); ++i)
+	{
+		ledstring.channel[0].leds[i] = value;
+	}
+	return 0;
+}
+
+void runTest()
+{
+	for (uint8_t b = 10; b < 50; ++b)
+	{
+		for (uint8_t g = 10; g < 50; ++g)
+		{
+			for ( uint8_t r = 10; r < 50; ++r)
+			{
+				// setLEDValue(getColorValue(r,g,b));
+				setNextLED(getColorValue(r,g,b));
+				ws2811_render(&ledstring);
+				// usleep(10);
+			}
+		}
+	}
+	setLEDValue(getColorValue(0,0,0));
 }
 
 
 int main(int argc, char *argv[])
 {
 	sprintf(VERSION, "%d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
+
 	parseargs(argc, argv, &ledstring);
 
     setup_handlers();
 
-#if 1
 	ws2811_init(&ledstring);
-	for (int i = 0; i < 32; ++i)
+
+	if (enableTest)
 	{
-			ledstring.channel[0].leds[i] = 0x00200000;
+		runTest();
+		exit(0);
 	}
+
+	setLEDValue(getColorValue(Red, Green, Blue));
 	ws2811_render(&ledstring);
-	sleep(1);
-	for (int i = 0; i < 32; ++i)
-	{
-			ledstring.channel[0].leds[i] = 0x00002000;
-	}
-	ws2811_render(&ledstring);
-	sleep(1);
-	for (int i = 0; i < 32; ++i)
-	{
-			ledstring.channel[0].leds[i] = 0x00000020;
-	}
-	ws2811_render(&ledstring);
-	sleep(1);
-	if (clear_on_exit)
-	{
-		for (int i = 0; i < (width * height); ++i)
-		{
-			ledstring.channel[0].leds[i] = 0x00000000;
-		}
-		ws2811_render(&ledstring);
-	}
+
 	ws2811_fini(&ledstring);
 	return 0;
-#else
-    ws2811_return_t ret;
-
-    matrix = malloc(sizeof(ws2811_led_t) * width * height);
-
-    if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS)
-    {
-        fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
-        return ret;
-    }
-
-    while (running)
-    {
-        matrix_raise();
-        matrix_bottom();
-        matrix_render();
-
-        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
-        {
-            fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
-            break;
-        }
-
-        // 15 frames /sec
-        usleep(1000000 / 15);
-    }
-
-    if (clear_on_exit) {
-	matrix_clear();
-	matrix_render();
-	ws2811_render(&ledstring);
-    }
-
-    ws2811_fini(&ledstring);
-
-    printf ("\n");
-    return ret;
-	#endif
 }
